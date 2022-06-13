d036fbf5bee5adfe2a8e7567c85b80c0379069a8
aanastasiyatuz <aanastasiyatuz@gmail.com>
2022-06-13 10:22:14 +0600

    if-else

--- basics/if-else.py
+++ basics/if-else.py
@@ -26,6 +26,33 @@
 '5' == 5 # False
 
 
+"=============And or not================="
+# and - и
+# or - или
+a = 5
+b = 6
+
+a == 5 and b == 6 # True (правая сторона True, левая тоже True)
+a == 5 and b == 5 # False (правая сторона True, но левая False)
+a == 4 and b == 5 # False (обе стороны False)
+
+a == 5 or b == 6 # True (правая сторона True, левая тоже True)
+a == 5 or b == 5 # True (правая сторона True, но левая False)
+a == 4 or b == 5 # False (обе стороны False)
+
+# если обе части выдают True - будет True
+# если обе части выдают False - будет False
+# если одна часть True, вторая False:
+# 1. если стоит and - выйдет False
+# 2. если стоит or - выйдет True
+
+not True # False
+not False # True
+not a == 5 # False (потому что a == 5)
+not a == 4 # True (потому что a == 5)
+
+
+
 "============Boolean Type================"
 # Булевый тип данных - имеет всего 2 значения True и False
 bool(1) # True
@@ -127,4 +154,11 @@ for i in range(1, 17):
         print(i)
 
 
+"==============Тернарные операторы================"
+# условия в одну строку
+тело1 if условие else тело2
 
+res = 'Hello' if a == 5 else 'Bye' 
+print(res) 
+# Hello, если a == 5
+# Bye, если a != 5
