dd59187c53ba05242f47ab4f0df083af8fea8c4f
aanastasiyatuz <aanastasiyatuz@gmail.com>
2022-07-21 10:19:35 +0600

    orm

---
+++ psql/orm.py
@@ -0,0 +1,6 @@
+"=================ORM==================="
+# ORM (Object-Relation Mapping) - библеотека, которая позволяет переводить питоновский код в sql запросы и обратно
+
+"ORM for Python"
+# SQLAlchemy
+# Peewee
